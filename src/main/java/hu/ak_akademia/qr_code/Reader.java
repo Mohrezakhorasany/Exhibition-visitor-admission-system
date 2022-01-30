f53ab006e2325e3eb5dab5ea79a1292cd82dc080
Mohrezakhorasany <m.reza.khorasany@gmail.com>
2022-01-30 15:24:03 +0100

    Added Reader class for QR code reading

---
+++ src/main/java/hu/ak_akademia/qr_code/Reader.java
@@ -0,0 +1,28 @@
+package hu.ak_akademia.qr_code;
+
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.Result;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
+
+import java.awt.image.BufferedImage;
+
+public class Reader {
+
+    public String read(BufferedImage bufferedImage) {
+        String encodedContent = null;
+        try {
+            BufferedImageLuminanceSource bufferedImageLuminanceSource = new BufferedImageLuminanceSource(bufferedImage);
+            HybridBinarizer hybridBinarizer = new HybridBinarizer(bufferedImageLuminanceSource);
+            BinaryBitmap binaryBitmap = new BinaryBitmap(hybridBinarizer);
+            MultiFormatReader multiFormatReader = new MultiFormatReader();
+            Result result = multiFormatReader.decode(binaryBitmap);
+            encodedContent = result.getText();
+        } catch (NotFoundException e) {
+            e.printStackTrace();
+        }
+        return encodedContent;
+    }
+}
