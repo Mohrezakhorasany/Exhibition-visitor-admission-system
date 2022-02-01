0170c5f57d68ad5621eb5a44d6e95657dec0961f
Mohrezakhorasany <m.reza.khorasany@gmail.com>
2022-02-01 13:59:58 +0100

    Added parent pom and fixed test cases

--- 1st maven homework/src/main/java/hu/ak_academy/spring/webdevelopment/Application.java
+++
@@ -1,7 +0,0 @@
-package hu.ak_academy.spring.webdevelopment;
-
-public class Application {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ 1st_homework/1st maven homework/src/main/java/hu/ak_academy/spring/webdevelopment/Application.java
@@ -0,0 +1,7 @@
+package hu.ak_academy.spring.webdevelopment;
+
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
