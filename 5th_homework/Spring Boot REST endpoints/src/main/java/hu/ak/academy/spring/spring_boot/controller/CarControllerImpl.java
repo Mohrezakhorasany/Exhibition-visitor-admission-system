757e85862f805bc9617ab930bf08d03db4e9f0f7
Mohrezakhorasany <m.reza.khorasany@gmail.com>
2022-02-22 19:03:38 +0100

    Adds @RestController, Adds appropriate annotations to HTTP methods requests in CarControllerImpl, removes application.yaml, removes unused dependencies

--- 5th_homework/Spring Boot REST endpoints/src/main/java/hu/ak/academy/spring/spring_boot/controller/CarControllerImpl.java
+++ 5th_homework/Spring Boot REST endpoints/src/main/java/hu/ak/academy/spring/spring_boot/controller/CarControllerImpl.java
@@ -1,30 +1,37 @@
 package hu.ak.academy.spring.spring_boot.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import hu.ak.academy.spring.spring_boot.controller.api.CarController;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/cars")
-public class CarControllerImpl implements hu.ak.academy.spring.spring_boot.controller.api.CarController {
+@RequestMapping(path = "/cars")
+@RestController
+public class CarControllerImpl implements CarController {
 
+    @GetMapping
     @Override
     public String getMapping() {
         return "This is get mapping method.";
     }
 
+    @PostMapping
     @Override
     public String postMapping() {
         return "This is post mapping method.";
     }
 
+    @PutMapping
     @Override
     public String putMapping() {
         return "This is put mapping method.";
     }
 
+    @PatchMapping
     @Override
     public String patchMapping() {
         return "This is patch mapping method.";
     }
 
+    @DeleteMapping
     @Override
     public String deleteMapping() {
         return "This is delete mapping method.";
