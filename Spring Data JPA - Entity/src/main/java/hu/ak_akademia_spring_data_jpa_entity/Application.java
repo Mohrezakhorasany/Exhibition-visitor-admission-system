1810a67cf02aefea1e247dbf471505f5ec5ba59a
Mohrezakhorasany <m.reza.khorasany@gmail.com>
2022-03-15 11:57:02 +0100

    Removes param of @SpringBootApplication

--- Spring Data JPA - Entity/src/main/java/hu/ak_akademia_spring_data_jpa_entity/Application.java
+++ Spring Data JPA - Entity/src/main/java/hu/ak_akademia_spring_data_jpa_entity/Application.java
@@ -3,7 +3,7 @@ package hu.ak_akademia_spring_data_jpa_entity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "hu.ak_akademia_spring_data_jpa_entity.repository")
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
