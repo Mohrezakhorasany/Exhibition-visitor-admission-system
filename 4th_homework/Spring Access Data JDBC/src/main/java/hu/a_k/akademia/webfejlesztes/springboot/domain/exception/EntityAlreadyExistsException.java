1199e59548e9b6f3cb0005fc155699127ab3525b
M.Reza Khorasany <47421833+Mohrezakhorasany@users.noreply.github.com>
2022-03-16 15:44:03 +0100

    Delete 4th_homework/Spring Access Data JDBC directory

--- 4th_homework/Spring Access Data JDBC/src/main/java/hu/a_k/akademia/webfejlesztes/springboot/domain/exception/EntityAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package hu.a_k.akademia.webfejlesztes.springboot.domain.exception;
-
-public class EntityAlreadyExistsException extends RuntimeException {
-    public EntityAlreadyExistsException(final String message, final Throwable cause) {
-        super(message, cause);
-    }
-}
